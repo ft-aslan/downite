c4373abed95825b25baf1b21be27251711f5945b
ft-aslan <fthaslncn@gmail.com>
2024-03-29 18:08:07 +0300

    modified prettier

--- src/components/ui/command.tsx
+++ src/components/ui/command.tsx
@@ -4,7 +4,7 @@ import { Command as CommandPrimitive } from "cmdk"
 import { Search } from "lucide-react"
 
 import { cn } from "@/lib/utils"
-import { Dialog, DialogContent } from "@/components/ui/ui/dialog"
+import { Dialog, DialogContent } from "@/components/ui/dialog"
 
 const Command = React.forwardRef<
   React.ElementRef<typeof CommandPrimitive>,
