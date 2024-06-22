fbbdcb9c75bc4cc0cf08005b273119c6d3d8047b
ft-aslan <fthaslncn@gmail.com>
2024-06-22 21:43:34 +0300

    fixed server startup bug. cause was we didn't set dialect for goose

--- apps/server/cmd/migrations/main.go
+++ apps/server/cmd/migrations/main.go
@@ -10,7 +10,10 @@ import (
 // Migrate runs all SQL migration files found in the migrations directory.
 func Migrate(db *sqlx.DB, migrationsDir string) error {
 	fmt.Printf("Migrating %s\n", migrationsDir)
-
+	//TODO(fatih): we need to embed migrations with go:embed
+	if err := goose.SetDialect("sqlite"); err != nil {
+		panic(err)
+	}
 	err := goose.Up(db.DB, migrationsDir)
 	if err != nil {
 		return err
